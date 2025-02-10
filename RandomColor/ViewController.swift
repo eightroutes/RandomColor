222bfb2287f37b777c8d38269872b9e43dadc372
EllGHT <52287330+eightroutes@users.noreply.github.com>
2025-02-10 14:33:18 +0900

    Initial Commit

---
+++ RandomColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColor
+//
+//  Created by 정근호 on 2/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
